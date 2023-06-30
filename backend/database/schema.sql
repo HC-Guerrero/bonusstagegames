0d99c0adfe0a86f85a2e3505240fc611cbcd729a
H.C.Guerrero <clydeguerrero1@gmail.com>
2023-06-30 16:16:11 -0400

    Created backend SQL files

---
+++ backend/database/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS bonus_stage_games;
+CREATE DATABASE bonus_stage_games;
+\c bonus_stage_games;
+
+CREATE TABLE videogames (
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL,
+genre TEXT,
+platforms TEXT,
+esrb_rating TEXT NOT NULL,
+release_date TEXT,
+price INT,
+about TEXT
+);
+
+CREATE TABLE users (
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL,
+photo TEXT,
+email TEXT NOT NULL,
+address TEXT,
+age INT NOT NULL,
+enrollment_date TEXT,
+membership BOOLEAN,
+payment_info FLOAT
+);
---
+++ backend/database/seed.sql
@@ -0,0 +1,7 @@
+\c bonus_stage_games;
+
+INSERT INTO videogames (name, genre, platforms, esrb_rating, release_date, price, about) VALUES
+('Starcraft', 'RTS', 'PC', 'T', 1996, 20, 'An epic rts game where the player assumes command of the tenacious and vicious Zerg, the resourceful and decentralized Terrans, and the technologically advanced and enigmatic Protoss.');
+
+INSERT INTO users (name, photo, email, address, age, enrollment_date, membership, payment_info) VALUES
+('Bob', 'axcwerqqwdz.jpg', 'bob1@gmail.com', '10-11-12th Grove Street', 32 , '01/01/2021', false, 4189609345129056  );
